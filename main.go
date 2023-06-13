c8b96b61f7ad8c3e678ea811fe1e09b61c195864
Mev Developer <126674880+Mevdevoloper@users.noreply.github.com>
2023-06-13 09:06:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to create bot:", err.Error())
 	}
+
 	updater := ext.NewUpdater(
 		&ext.UpdaterOpts{
 			ErrorLog: nil,
@@ -44,6 +45,7 @@ func main() {
 	// Commands
 	dp.AddHandler(handlers.NewCommand("start", Start))
 	dp.AddHandler(handlers.NewChatJoinRequest(nil, Approve))
+	dp.AddHandler(handlers.NewCommand("status", Status))
 
 	// Start Polling()
 	poll := updater.StartPolling(bot, &ext.PollingOpts{DropPendingUpdates: true})
@@ -51,7 +53,7 @@ func main() {
 		fmt.Println("Failed to start bot:", poll.Error())
 	}
 
-	fmt.Printf("@%s has been sucesfully started\n💝Made by @CodeMasterTG\n", bot.Username)
+	fmt.Printf("@%s has been successfully started\n💝Made by @CodeMasterTG\n", bot.Username)
 	updater.Idle()
 }
 
@@ -63,22 +65,21 @@ func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
 	user := ctx.EffectiveSender.User
 	text := `
 <b>Hello <a href="tg://user?id=%v">%v</a></b> ❤️
-I am a bot made for accepting newly coming join requests at the time they comes.
+I am a bot made for accepting newly coming join requests at the time they come.
 
 Bot made with 💝 by <a href="t.me/CodeMasterTG">Code Master Bots</a> for you!
-<b>Support Chat:</b><a href="t.me/+4KDIm0IQ_NQ0NDdl">Support Chat</a>
+<b>Support Chat:</b> <a href="t.me/+4KDIm0IQ_NQ0NDdl">Support Chat</a>
 	`
 	ctx.EffectiveMessage.Reply(
-    bot,
-    fmt.Sprintf(text, user.Id, user.FirstName),
-    &gotgbot.SendMessageOpts{
-        ParseMode:             "html",
-        DisableWebPagePreview: true,
-    },
-)
-return nil
+		bot,
+		fmt.Sprintf(text, user.Id, user.FirstName),
+		&gotgbot.SendMessageOpts{
+			ParseMode:             "html",
+			DisableWebPagePreview: true,
+		},
+	)
+	return nil
 }
-
 
 func Approve(bot *gotgbot.Bot, ctx *ext.Context) error {
 	_, err := bot.ApproveChatJoinRequest(ctx.EffectiveChat.Id, ctx.EffectiveSender.User.Id)
@@ -87,3 +88,13 @@ func Approve(bot *gotgbot.Bot, ctx *ext.Context) error {
 	}
 	return nil
 }
+
+func Status(bot *gotgbot.Bot, ctx *ext.Context) error {
+	// Get the number of users in the chat
+	count, err := bot.GetChatMembersCount(ctx.EffectiveChat.Id)
+	if err != nil {
+		fmt.Println("Error while getting chat members count:", err.Error())
+		return nil
+	}
+
+	response := fmt.Sprintf("Number of users in the chat: %d",
